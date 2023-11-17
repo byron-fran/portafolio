23f04eebcfa9ea48e705d3e9fa8fb15e37e2c405
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2023-11-17 12:14:32 -0800

    in_propress_v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import {Routes, Route} from 'react-router-dom'
 import Header from './pages/Header';
 import Home from './pages/Home';
-
+import Componente1 from './components/Componente1';
 function App() {
 
 
   return (
     <>
-      
-      <Header/>
+      <h1>Hola mundo</h1>
+      {/* <Header/> */}
+      <Componente1/>
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
